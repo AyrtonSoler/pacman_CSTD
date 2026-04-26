05c2994ac564fab22b31b5d9a2e6ae444e3ba992
Franco De E <escondrillas.franco@gmail.com>
2026-04-25 21:05:07 -0600

    Pacman dot-eating illusion finally fixed
    Finally fixed the bug for drawing the pacman route with a black line, giving the illusion of it eating the dots.

--- Pacman.py
+++ Pacman.py
@@ -47,14 +47,7 @@ class Pacman:
         # Move | UP(1), RIGHT(2), DOWN(3) or LEFT(4)
         if self.dir:
             self.pos[(self.dir & 1)] += 1 if (self.dir & 2) else -1
-        self.route()    # <--
-        for key in self.xMap:
-            print(len(self.xMap[key]), end = ' ')
-        print()
-        for key in self.yMap:
-            print(len(self.yMap[key]), end = ' ')
-        print()
-        print()
+        self.route()
         return
 
     def draw(self, open):
@@ -94,84 +87,50 @@ class Pacman:
     def mapdraw(self, key, segment, horizontal):
         glColor3f(0,0,0)
         glBegin(GL_QUADS)
-        shift, lmargin, rmargin = 15, 15, 25
+        lmargin, rmargin = 15, 25
         if horizontal:
-            glVertex2d(segment[0] + shift, key + lmargin)
-            glVertex2d(segment[1] + shift, key + lmargin)
-            glVertex2d(segment[1] + shift, key + rmargin)
-            glVertex2d(segment[0] + shift, key + rmargin)
+            glVertex2d(segment[0] + lmargin, key + lmargin)
+            glVertex2d(segment[1] + rmargin, key + lmargin)
+            glVertex2d(segment[1] + rmargin, key + rmargin)
+            glVertex2d(segment[0] + lmargin, key + rmargin)
         else:
-            glVertex2d(key + lmargin, segment[0] + shift)
-            glVertex2d(key + lmargin, segment[1] + shift)
-            glVertex2d(key + rmargin, segment[1] + shift)
-            glVertex2d(key + rmargin, segment[0] + shift)
+            glVertex2d(key + lmargin, segment[0] + lmargin)
+            glVertex2d(key + lmargin, segment[1] + rmargin)
+            glVertex2d(key + rmargin, segment[1] + rmargin)
+            glVertex2d(key + rmargin, segment[0] + lmargin)
         glEnd()
         return
 
     def route(self):
-        x = self.pos[0]
-        y = self.pos[1]
-        sz = 20 if self.dir & 2 else 0
-        if self.dir & 1:
-            for idx, segment in enumerate(self.xMap[x]):
-                # Insert segment before
-                if y < segment[0] - 1:
-                    self.xMap[x].insert(idx, [y, y])
-                    break
-                # Coords belong to this segment
-                if y == segment[0] - 1:
-                    segment[0] -= 1
-                    # Join with prev
-                    if(idx > 0):
-                        if(self.xMap[x][idx - 1][1] >= segment[0]):
-                            self.xMap[x][idx - 1][1] = segment[1]
-                            del self.xMap[x][idx]
-                    break
-                if y == segment[1] + 1:
-                    segment[1] += 26
-                    # Join with next
-                    if(idx < len(self.xMap[x]) - 1):
-                        if(segment[1] >= self.xMap[x][idx + 1][0]):
-                            self.xMap[x][idx][1] = self.xMap[x][idx + 1][1]
-                            del self.xMap[x][idx + 1]
-                    break
-                if y >= segment[0] and y <= segment[1]:
-                    break
-            # Insert segment after
-            if len(self.xMap[x]) == 0:
-                self.xMap[x].append([y, y])
-            elif y > self.xMap[x][-1][1] + 1:
-                self.xMap[x].append([y, y])
-        else:
-            for idx, segment in enumerate(self.yMap[y]):
-                # Insert segment before
-                if x < segment[0] - 1:
-                    self.yMap[y].insert(idx, [x, x])
-                    break
-                # Coords belong to this segment
-                if x == segment[0] - 1:
-                    segment[0] -= 1
-                    # Join with prev
-                    if(idx > 0):
-                        if(self.yMap[y][idx - 1][1] >= segment[0]):
-                            self.yMap[y][idx - 1][1] = segment[1]
-                            del self.yMap[y][idx]
-                    break
-                if x == segment[1] + 1:
-                    segment[1] += 26
-                    # Join with next
-                    if(idx < len(self.yMap[y]) - 1):
-                        if(segment[1] >= self.yMap[y][idx + 1][0]):
-                            self.yMap[y][idx][1] = self.yMap[y][idx + 1][1]
-                            del self.yMap[y][idx + 1]
-                    break
-                if x > segment[0] and x < segment[1]:
-                    break
-            # Insert segment after
-            if len(self.yMap[y]) == 0:
-                self.yMap[y].append([x, x])
-            elif x > self.yMap[y][-1][1] + 1:
-                self.yMap[y].append([x, x])
+        sz = 20
+        x, y = self.pos[0], self.pos[1]
+        map = self.xMap[x] if self.dir & 1 else self.yMap[y]
+        p = y if self.dir & 1 else x
+
+        for idx, segment in enumerate(map):
+            if p < segment[0] - 1:
+                map.insert(idx, [p, p])
+                break
+            if p == segment[0] - 1:
+                segment[0] -= 1
+                break
+            if segment[0] <= p and p <= segment[1]:
+                break
+            if p == segment[1] + 1:
+                segment[1] += 1
+                break
+
+        if not len(map):
+            map.append([p, p])
+        elif p > map[-1][1]:
+            map.append([p, p])
+
+        for idx in range(len(map) - 1):
+            if map[idx][1] + 1 >= map[idx + 1][0]:
+                map[idx][1] = map[idx + 1][1]
+                del map[idx + 1]
+                break
+
         # Draw rectangles
         for key in self.xMap:
             for segment in self.xMap[key]:
